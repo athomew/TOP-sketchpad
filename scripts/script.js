48abb1b1eec9b1a3c1179d3ca2dca5bcf80760d7
Aline Thome <alinethomew@gmail.com>
2017-07-02 04:09:35 -0500

    Make 16x16 grid.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    $('body').html("Testing. 1. 2. 3.")
+    makeGrid(16);
 });
+
+function makeGrid(size){
+    $('body').append('<div id="container"></div>');
+    for (var i=1; i<=size; i++) {
+        makeRow(size, i);
+    }
+};
+
+function makeRow(size, n){
+    $('#container').append('<div class="row" id="row'+ n + '"></div>');
+    for (var i=1; i<=size; i++) {
+            makeTile(n, i);
+        }
+}
+
+function makeTile(n, m){
+        var curRowID = "#row" + n.toString();
+        $(curRowID).append('<div class="tile" id="tile'+ n + 'x' + m + '"></div>');
+}
