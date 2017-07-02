d7ea0bfde0c16ed43c00eb71f1ac0e7b2e85ef0b
Aline Thome <alinethomew@gmail.com>
2017-07-02 16:47:10 -0500

    Add resizing functionality. Prettify it a little.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
+var gridSize = 16;
+var tileSize = 25;
+var tileSizeStr = tileSize.toString() + 'px';
+
 $(document).ready(function(){
-    makeGrid(16);
+    makeGrid(gridSize);
     $(document).on('mouseenter','.tile', function(){
         $(this).addClass('filled');
     })
+    $(document).on('click','#resize-button', function(){
+        $('#container').remove();
+        gridSize = $('#grid-size').val();
+        tileSize = (400/gridSize);
+        tileSizeStr = tileSize.toString() + 'px';
+        makeGrid(gridSize);
+    });
 });
 
 function makeGrid(size){
@@ -10,6 +21,9 @@ function makeGrid(size){
     for (var i=1; i<=size; i++) {
         makeRow(size, i);
     }
+    $('.row').height(tileSizeStr);
+    $('.tile').height(tileSizeStr);
+    $('.tile').width(tileSizeStr);
 };
 
 function makeRow(size, n){
@@ -22,4 +36,8 @@ function makeRow(size, n){
 function makeTile(n, m){
         var curRowID = "#row" + n.toString();
         $(curRowID).append('<div class="tile" id="tile'+ n + 'x' + m + '"></div>');
+}
+
+function resize(){
+
 }
